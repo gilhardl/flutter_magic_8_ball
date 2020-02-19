ed140eadf01440b788cd8559867ff14966fa3c74
Lucas GILHARD <l.gilhard@gmail.com>
2020-02-19 10:20:37 +0100

    add features

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,50 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        title: Text('Ask me anything'),
+        backgroundColor: Colors.blue[900],
+      ),
+      body: Center(
+        child: Ball(),
+      ),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 3;
+
+  void shuffleBall() {
+    setState(() {
+      ballNumber = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FlatButton(
+      child: Image.asset('images/ball$ballNumber.png'),
+      onPressed: () {
+        shuffleBall();
+      },
+    );
+  }
+}
